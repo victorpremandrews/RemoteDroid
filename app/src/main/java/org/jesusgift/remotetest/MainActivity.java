6321be2a610a5e401f87e0c371c474d301d0831f
victorpremandrews <victorpremandrews@gmail.com>
2016-03-29 18:52:48 +0530

    Initial Commit

---
+++ app/src/main/java/org/jesusgift/remotetest/MainActivity.java
@@ -0,0 +1,71 @@
+package org.jesusgift.remotetest;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.MediaRouteActionProvider;
+import android.support.v7.media.MediaControlIntent;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.cast.Cast;
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaRouter mediaRouter;
+    private MediaRouteSelector mediaRouteSelector;
+    private MediaRouter.Callback mCallback;
+    private CastDevice mDevice;
+    private String appId = "0926AB23";
+
+    private static final String TAG = "MainActivity";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaRouter = MediaRouter.getInstance(this);
+        mediaRouteSelector = new MediaRouteSelector.Builder()
+                        .addControlCategory(CastMediaControlIntent.categoryForCast(appId))
+                        .build();
+        mCallback = new MediaCallback();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mediaRouter.addCallback(mediaRouteSelector, mCallback, MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem mediaRouteMenuItem = menu.findItem(R.id.media_route_menu_item);
+        MediaRouteActionProvider mediaRouteActionProvider =
+                (MediaRouteActionProvider) MenuItemCompat.getActionProvider(mediaRouteMenuItem);
+        mediaRouteActionProvider.setRouteSelector(mediaRouteSelector);
+        return true;
+    }
+
+    private final class MediaCallback extends MediaRouter.Callback{
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo route) {
+            Log.d(TAG, "Route Selected");
+            mDevice = CastDevice.getFromBundle(route.getExtras());
+            String routeId = route.getId();
+            Log.d(TAG, "Route ID : "+routeId);
+        }
+
+        @Override
+        public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo route) {
+            Log.d(TAG, "Route Unselected");
+            mDevice = null;
+        }
+    }
+}
